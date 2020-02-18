d6fc4eae6ed28c92d189aeea538f557d246bd6d2
DanielJBeckman <dbeckman@usgs.gov>
2020-02-18 14:41:52 -0700

    Small build.sh fixes

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ sudo apt-get install -y python3-pip
 sudo apt-get install -y python3-numpy
 sudo apt-get install -y python3-scipy
 sudo apt install -y libatlas-base-dev
-sudo pip3 install -y wrapt --upgrade --ignore-installed
-sudo pip3 install -y h5py
-sudo pip3 install -y tensorflow
-sudo pip3 install -y keras
-sudo apt-get install -y python3-opencv
+sudo pip3 install wrapt --upgrade --ignore-installed
+sudo pip3 install h5py
+sudo pip3 install tensorflow
+sudo pip3 install keras
+sudo apt install -y python3-opencv
